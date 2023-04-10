9aac9e55dbc49ff04eb21099491a505340ae25bc
Daegyeom Ha <combbm@gmail.com>
2023-04-10 22:35:54 +0900

    Project init

---
+++ src/main/java/leets/crazyform/CrazyformBackendApplication.java
@@ -0,0 +1,13 @@
+package leets.crazyform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrazyformBackendApplication.class, args);
+    }
+
+}
