203e9277a40db194979102185735f985e72b50a8
Daegyeom Ha <combbm@gmail.com>
2023-04-13 18:29:49 +0900

    feat: Add EnableJpaAuditing annotaton
    To add createdAt and updatedAt columns

--- src/main/java/leets/crazyform/CrazyformBackendApplication.java
+++ src/main/java/leets/crazyform/CrazyformBackendApplication.java
@@ -2,8 +2,10 @@ package leets.crazyform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CrazyformBackendApplication {
 
     public static void main(String[] args) {
