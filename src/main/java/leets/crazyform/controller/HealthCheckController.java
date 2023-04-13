0db4f057443498c8b68c590ddeebece28f2f46a0
Daegyeom Ha <combbm@gmail.com>
2023-04-13 18:27:58 +0900

    feat: Spring boot init

---
+++ src/main/java/leets/crazyform/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package leets.crazyform.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public boolean healthCheck() {
+        return true;
+    }
+}
