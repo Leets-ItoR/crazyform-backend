f47f02dab2b7547c1662416d451a159805da3229
Daegyeom Ha <combbm@gmail.com>
2023-04-29 20:17:54 +0900

    fix: Remove unused annotation

--- src/main/java/leets/crazyform/app/shared/entity/BaseTimeEntity.java
+++ src/main/java/leets/crazyform/app/shared/entity/BaseTimeEntity.java
@@ -1,6 +1,5 @@
 package leets.crazyform.app.shared.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -15,7 +14,6 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public class BaseTimeEntity {
     @CreatedDate
-    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
