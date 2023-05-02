41ab6239145f38d9c0a052192e859f8bb787a2e6
Daegyeom Ha <combbm@gmail.com>
2023-05-02 17:00:10 +0900

    feat: 일반 유저 JWT 관련 class 구현
    * JWtProvider을 통해 토큰을 만들거나 토큰 안의 값을 가져옵니다.
    * AuthDetails 그리고 서비스를 통해 인증에서 사용하는 유저 객체를 관리합니다.
    * JwtFilter을 통해 API 요청 시 JWT 인증을 적용합니다.

---
+++ src/main/java/leets/crazyform/global/jwt/JwtProvider.java
@@ -0,0 +1,63 @@
+package leets.crazyform.global.jwt;
+
+import io.jsonwebtoken.*;
+import leets.crazyform.global.jwt.detail.AuthDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtProvider {
+    private final String secret;
+    private final AuthDetailsService authDetailsService;
+
+    @Autowired
+    public JwtProvider(@Value("${jwt.secret}") String secret, AuthDetailsService authDetailsService) {
+        this.secret = secret;
+        this.authDetailsService = authDetailsService;
+    }
+
+    public String generateToken(Authentication authentication, boolean isRefreshToken) {
+        Instant accessDate = LocalDateTime.now().plusHours(6).atZone(ZoneId.systemDefault()).toInstant();
+        Instant refreshDate = LocalDateTime.now().plusDays(30).atZone(ZoneId.systemDefault()).toInstant();
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        return Jwts.builder()
+                .claim("role", authorities)
+                .setSubject(authentication.getName())
+                .setExpiration(isRefreshToken ? Date.from(refreshDate) : Date.from(accessDate))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = parseClaims(token);
+        Collection<? extends GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(claims.get("role").toString()));
+        UserDetails principal = this.authDetailsService.loadUserByUsername(claims.getSubject());
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try {
+            JwtParser parser = Jwts.parser().setSigningKey(secret);
+            return parser.parseClaimsJws(accessToken).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+}
