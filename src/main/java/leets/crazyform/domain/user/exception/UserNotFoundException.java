d3b44d55bbe03666f804786d5d0858e6f580333d
Daegyeom Ha <combbm@gmail.com>
2023-05-02 18:34:22 +0900

    fix: UserNotFoundException 생성자 접근제어를 public으로 변경

--- src/main/java/leets/crazyform/domain/user/exception/UserNotFoundException.java
+++ src/main/java/leets/crazyform/domain/user/exception/UserNotFoundException.java
@@ -4,7 +4,7 @@ import leets.crazyform.global.error.ErrorCode;
 import leets.crazyform.global.error.exception.ServiceException;
 
 public class UserNotFoundException extends ServiceException {
-    private UserNotFoundException() {
+    public UserNotFoundException() {
         super(ErrorCode.USER_NOT_FOUND);
     }
 }
