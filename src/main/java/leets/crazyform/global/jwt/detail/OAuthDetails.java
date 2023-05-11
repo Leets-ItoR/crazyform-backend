ebcca3704c8b6b4de4e5eb24379280211d7e8018
Daegyeom Ha <combbm@gmail.com>
2023-05-11 18:30:02 +0900

    feat: OAuth용 UserDetails 추가

---
+++ src/main/java/leets/crazyform/global/jwt/detail/OAuthDetails.java
@@ -0,0 +1,66 @@
+package leets.crazyform.global.jwt.detail;
+
+import leets.crazyform.global.jwt.AuthRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class OAuthDetails implements UserDetails, OAuth2User {
+    private final String email;
+    private final String name;
+    private final Map<String, Object> attributes;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(AuthRole.ROLE_PARTICIPANT.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
