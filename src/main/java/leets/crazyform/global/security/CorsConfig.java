ab3b44b9187b2791a5956324480f3fe7733091fc
Daegyeom Ha <combbm@gmail.com>
2023-05-18 18:50:35 +0900

    HOTFIX: CORS 오류 수정

--- src/main/java/leets/crazyform/global/security/CorsConfig.java
+++ src/main/java/leets/crazyform/global/security/CorsConfig.java
@@ -1,13 +1,17 @@
 package leets.crazyform.global.security;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableWebMvc
+@Slf4j
 public class CorsConfig implements WebMvcConfigurer {
 
     private String developmentOrigin;
@@ -22,6 +26,6 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                .allowedOrigins(developmentOrigin, productionOrigin);
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5175");
     }
 }
