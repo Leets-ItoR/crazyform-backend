74caa7b6b221ef0fc04a142a507cf9e0b2b07526
Daegyeom Ha <combbm@gmail.com>
2023-05-18 15:30:54 +0900

    fix: CORS 환경변수 주입 오류 수정

--- src/main/java/leets/crazyform/global/security/CorsConfig.java
+++ src/main/java/leets/crazyform/global/security/CorsConfig.java
@@ -1,18 +1,22 @@
 package leets.crazyform.global.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${cors.host.development}")
     private String developmentOrigin;
-
-    @Value("${cors.host.production}")
     private String productionOrigin;
+
+    public CorsConfig(@Value("${cors.host.development}") String developmentOrigin, @Value("${cors.host.production}") String productionOrigin) {
+        this.developmentOrigin = developmentOrigin;
+        this.productionOrigin = productionOrigin;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
