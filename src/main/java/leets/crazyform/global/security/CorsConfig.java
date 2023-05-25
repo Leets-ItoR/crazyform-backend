1abc1fcb8473a3c26e7409e69b0804184d8b8ee9
Daegyeom Ha <combbm@gmail.com>
2023-05-25 17:00:18 +0900

    fix: CORS 오류 수정

--- src/main/java/leets/crazyform/global/security/CorsConfig.java
+++ src/main/java/leets/crazyform/global/security/CorsConfig.java
@@ -1,31 +1,30 @@
 package leets.crazyform.global.security;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@RequiredArgsConstructor
 @Configuration
 @EnableWebMvc
-@Slf4j
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${cors.host.development}")
     private String developmentOrigin;
-    private String productionOrigin;
 
-    public CorsConfig(@Value("${cors.host.development}") String developmentOrigin, @Value("${cors.host.production}") String productionOrigin) {
-        this.developmentOrigin = developmentOrigin;
-        this.productionOrigin = productionOrigin;
-    }
+    @Value("${cors.host.production}")
+    private String productionOrigin;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5175");
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:5173",
+                        "http://localhost:5174",
+                        "http://localhost:5175",
+                        developmentOrigin,
+                        productionOrigin);
     }
 }
