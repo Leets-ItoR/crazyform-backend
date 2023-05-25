60c1234a51ed11494bb7441da8cc09434bb8e0cb
Daegyeom Ha <combbm@gmail.com>
2023-05-26 00:30:01 +0900

    feat: User와 Creator 분리
    * User는 설문참여자를 Creator는 설문제작자를 말합니다.

--- src/main/java/leets/crazyform/domain/creator/domain/Creator.java
+++ src/main/java/leets/crazyform/domain/creator/domain/Creator.java
@@ -1,36 +1,35 @@
-package leets.crazyform.domain.user.domain;
+package leets.crazyform.domain.creator.domain;
 
 import jakarta.annotation.PreDestroy;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import leets.crazyform.domain.shared.entity.BaseTimeEntity;
-import leets.crazyform.domain.user.type.Vendor;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Entity(name = "users")
-@SQLDelete(sql = "UPDATE users SET deleted_at=now() where id=?")
+@Entity(name = "creators")
+@SQLDelete(sql = "UPDATE creators SET deleted_at=now() where id=?")
 @Where(clause = "deleted_at IS NULL")
-public class User extends BaseTimeEntity {
+public class Creator extends BaseTimeEntity {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "BINARY(16)")
     private UUID id;
 
-    @Column(columnDefinition = "CHAR(10)")
-    @Enumerated(EnumType.STRING)
-    private Vendor vendor;
+    @Column()
+    private String password;
 
     @Column(nullable = false)
     private String email;
