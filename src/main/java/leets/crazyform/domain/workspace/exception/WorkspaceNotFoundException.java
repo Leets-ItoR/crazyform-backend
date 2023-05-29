1e54c837db188699a4b3badb6beb0151f348178d
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-05-29 21:33:44 +0900

    Update src/main/java/leets/crazyform/domain/workspace/exception/WorkspaceNotFoundException.java
    Co-authored-by: Daegyeom Ha <combbm@gmail.com>

--- src/main/java/leets/crazyform/domain/workspace/exception/WorkspaceNotFoundException.java
+++ src/main/java/leets/crazyform/domain/workspace/exception/WorkspaceNotFoundException.java
@@ -4,7 +4,7 @@ import leets.crazyform.global.error.ErrorCode;
 import leets.crazyform.global.error.exception.ServiceException;
 
 public class WorkspaceNotFoundException extends ServiceException {
-    public WorkspaceNotFoundException(String s) {
+    public WorkspaceNotFoundException() {
         super(ErrorCode.WORKSPACE_NOT_FOUND);
     }
 }
